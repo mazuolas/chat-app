24beb116843a7db5a42f60ac41d5bab78927b3e6
Mark Azuolas <markazuolas@gmail.com>
2017-09-26 15:28:58 -0700

    save dom elements

--- public/javascripts/chatUI.js
+++ public/javascripts/chatUI.js
@@ -2,5 +2,8 @@ import './chat.js';
 class ChatUI {
   constructor(socket){
     this.chat = new Chat(socket)
+    this.room = document.getElementById('room');
+    this.msgList = document.getElementById('msg-list');
+    this.roomList = document.getElementById('room-list');
   }
 }
