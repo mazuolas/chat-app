4b02468dae4be8e236c62323533791c127a39bd4
Mark Azuolas <markazuolas@gmail.com>
2017-09-26 15:31:01 -0700

    add input

--- public/javascripts/chatUI.js
+++ public/javascripts/chatUI.js
@@ -5,5 +5,7 @@ class ChatUI {
     this.room = document.getElementById('room');
     this.msgList = document.getElementById('msg-list');
     this.roomList = document.getElementById('room-list');
+    this.input = document.querySelector('input');
   }
+
 }
