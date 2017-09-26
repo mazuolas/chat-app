65a8209db834e09933dec80c7659a9bd95a51e8c
Mark Azuolas <markazuolas@gmail.com>
2017-09-26 15:46:14 -0700

    create chat ui

--- public/javascripts/chatUI.js
+++ public/javascripts/chatUI.js
@@ -1,12 +1,18 @@
-import './chat.js';
+import Chat from './chat.js';
 class ChatUI {
   constructor(socket){
+    console.log('chat ui created');
     this.chat = new Chat(socket)
     this.room = document.getElementById('room');
     this.msgList = document.getElementById('msg-list');
     this.roomList = document.getElementById('room-list');
     this.input = document.querySelector('input');
   }
+  addMsg(msg){
+    let newMsg = document.createElement('li');
 
+    newMsg.textContent = msg;
+    this.msgList.addChild(newMsg);
+  }
 }
 export default ChatUI;
--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,1 +1,7 @@
-const io = require('socket.io-client')();
+import ChatUI from './chatUI.js';
+document.addEventListener('DOMContentLoaded', () => {
+  const io = require('socket.io-client')();
+  console.log(ChatUI);
+  const myChat = new ChatUI(io);
+  myChat.addMsg('hello');
+})
